9bde98de42187ddb073ace91d280d74f374ee7d7
Vitor Freitas <vitorfs@gmail.com>
2013-07-27 21:30:20 -0300

    Adicionando o plugin

---
+++ gerundator.js
@@ -0,0 +1,22 @@
+(function ($) {
+    $.fn.disable = function (action) {
+        var text = $(this).html();
+        if (action) {
+            if (text.substr(text.length - 1) == "r")
+                text = text.substr(0, text.length - 1) + "ndo";
+            text += "...";
+        }
+        else {
+            if (text.substr(text.length - 3) == "...")
+                text = text.substr(0, text.length - 3);
+            if (text.substr(text.length - 3) == "ndo")
+                text = text.substr(0, text.length - 3) + "r";
+        }
+        $(this).html(text);
+        $(this).attr("disabled", action);
+    };
+
+    $.fn.toggleDisable = function () {
+        $(this).disable(!$(this).is(":disabled"));
+    };
+}(jQuery));
